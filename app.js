49d9f8f377836c933da3efa02c71606aa61abdb3
GhofranAljabr <ghofran.gorro@gmail.com>
2021-06-06 13:51:14 -0700

    do the new repo

---
+++ app.js
@@ -0,0 +1,21 @@
+
+let userNAme = prompt('what is your Name ?');
+console.log(userNAme);
+
+alert(`Welcome ${userNAme}`);
+console.log(`Welcome ${userNAme}`);
+
+let flower = prompt('what is your favorite flower ?');
+console.log(flower);
+
+alert(`your favorite floweris ${flower}`);
+console.log(`wow it is ${flower}`);
+
+let color = prompt('what is your favorite color ?');
+console.log(color);
+
+alert(`your favorite color is ${color}`);
+console.log(`wow it is ${colore}`);
+
+
+
