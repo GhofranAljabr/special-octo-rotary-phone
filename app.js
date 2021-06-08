0b087deebe8e21b9f15c60173ca905aac51772c6
GhofranAljabr <ghofran.gorro@gmail.com>
2021-06-08 07:40:28 -0700

    add Q

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ console.log(`Welcome ${userNAme}`);
 let flower = prompt('what is your favorite flower ?');
 console.log(flower);
 
-alert(`your favorite floweris ${flower}`);
+alert(`your favorite flower is ${flower}`);
 console.log(`wow it is ${flower}`);
 
 let color = prompt('what is your favorite color ?');
 console.log(color);
 
 alert(`your favorite color is ${color}`);
-console.log(`wow it is ${colore}`);
+console.log(`wow it is ${color}`);
+
+let Season = prompt('what is your favorite Season ?');
+console.log(Season);
+
+alert(`your favorite Season is ${Season}`);
+console.log(`wow it is ${Season}`);
 
 
 
